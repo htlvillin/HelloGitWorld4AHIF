2c96ee91f3400a6d13815d12100c044c07882c76
nlinder <nicki.linder@gmail.com>
2019-03-26 11:03:39 +0100

    Initial import

---
+++ app/src/main/java/at/htl_villach/myfirstapplication_4ahif/MainActivity.java
@@ -0,0 +1,13 @@
+package at.htl_villach.myfirstapplication_4ahif;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
